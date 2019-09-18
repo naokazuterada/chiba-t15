3824980a4a1c6ec54105b57fb85cf3f9bf12bb81
Naokazu Terada <naokazu.terada@gmail.com>
2019-09-19 02:07:28 +0900

    show days

---
+++ components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrap">
+    <span class="pre">台風から</span>
+    <span class="num">{{ day }}</span>
+    <span class="days">日目</span>
+  </div>
+</template>
+
+<style>
+.wrap {
+  display: flex;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  text-align: center;
+  font-family: "游ゴシック" , "Yu Gothic" , "游ゴシック体" , "YuGothic" , "ヒラギノ角ゴ Pro W3" , "Hiragino Kaku Gothic Pro" , "Meiryo UI" , "メイリオ" , Meiryo , "ＭＳ Ｐゴシック" , "MS PGothic" , sans-serif;
+}
+.pre,
+.days {
+  font-size: 12vw;
+  font-weight: bold;
+}
+.num {
+  font-size: 65vw;
+  line-height: 1em;
+}
+</style>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    day: {
+      type: String,
+      required: true
+    }
+  },
+  mounted: function(){
+    this.day = moment().diff(moment('2019-09-09'), 'days');
+  }
+}
+</script>
